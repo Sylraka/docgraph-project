0595d62ab1521314e42a23d821da7f8edd124272
Alice <ruehle.alice@gmail.com>
2024-09-23 16:12:20 +0200

    text follows setWidth and setHeight

--- react/src/pages/single-board/elements/cardText.tsx
+++ react/src/pages/single-board/elements/cardText.tsx
@@ -23,20 +23,61 @@ export default function CardTextComponent(props: canvasProps) {
 
     const [element, setElement] = useState<Card>({
         ...props.card,
-        x: props.card.x +60
+        x: props.card.x + 60
     });
-    
-    useEffect(() => {
-        setTextPosition();
-    },[activeDragValue])
 
+
+    //update width and height of a card (cardFocus is moving)
+    useEffect(() => {
+        if(activeDragValue.elementType === "card"){
+            setTextPosition();
+        }
+
+
+        if (activeDragValue.elementType === "cardAnchorBottomRight" && activeDragValue.ID == element.cardID) {
+            setElement(prevElement => ({
+                ...prevElement,
+                width: Math.max(prevElement.width + activeDragValue.width, 30),
+                height: Math.max(prevElement.height + activeDragValue.height, 30)
+            }))
+
+        } else if (activeDragValue.elementType === "cardAnchorBottomLeft" && activeDragValue.ID == element.cardID) {
+            setElement(prevElement => ({
+                ...prevElement,
+                x: prevElement.x + activeDragValue.width,
+                width: Math.max(prevElement.width - activeDragValue.width, 30),
+                height: Math.max(prevElement.height + activeDragValue.height, 30)
+            }))
+
+        } else if (activeDragValue.elementType === "cardAnchorTopRight" && activeDragValue.ID == element.cardID) {
+            setElement(prevElement => ({
+                ...prevElement,
+                y: prevElement.y + activeDragValue.height,
+                width: Math.max(prevElement.width + activeDragValue.width, 30),
+                height: Math.max(prevElement.height - activeDragValue.height, 30)
+            }))
+
+        } else if (activeDragValue.elementType === "cardAnchorTopLeft" && activeDragValue.ID == element.cardID) {
+            setElement(prevElement => ({
+                ...prevElement,
+                y: prevElement.y + activeDragValue.height,
+                x: prevElement.x + activeDragValue.width,
+                width: Math.max(prevElement.width - activeDragValue.width, 30),
+                height: Math.max(prevElement.height - activeDragValue.height, 30)
+            }))
+
+        }
+
+
+
+    }, [activeDragValue]);
 
 
 
 
     const setTextPosition = () => {
-    
-        if (activeDragValue.ID === props.card.cardID && activeDragValue.elementType === "card"){
+
+        if (activeDragValue.ID === props.card.cardID && activeDragValue.elementType === "card") {
             setElement((prevElement) => ({
                 ...prevElement,
                 x: activeDragValue.placeToLeftX + 60,
@@ -45,13 +86,13 @@ export default function CardTextComponent(props: canvasProps) {
         }
     }
 
-    return(
+    return (
         <p
-        key={props.card.cardID.toString()}
-        className='text-element'
-        style={{ 'top': element.y, 'left': element.x, 'width': element.width - 10, 'height': element.height - 40 }}
-    >
-        {element.text}
-    </p>
+            key={props.card.cardID.toString()}
+            className='text-element'
+            style={{ 'top': element.y, 'left': element.x, 'width': element.width - 10, 'height': element.height - 40 }}
+        >
+            {element.text}
+        </p>
     );
 }
